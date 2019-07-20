b723e3b9b494ac3bad866d45c85ebead533700b6
Leandro Arzolla Coelho <leandrocoelho@DA00356.da.sa>
2019-07-20 15:48:34 -0300

    adds cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 
 import './database';
 import routes from './routes';
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
