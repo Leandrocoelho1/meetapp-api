12383a22daaebe1028a1b9a40aeec97a4571bfdf
Leandro Arzolla Coelho <leandrocoelho@DA00356.da.sa>
2019-07-20 18:38:41 -0300

    adds file upload

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import { resolve } from 'path';
 
 import './database';
 import routes from './routes';
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(resolve(__dirname, '..', 'tmp', 'uploads')));
 app.use(routes);
 
 export default app;
